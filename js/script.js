4bf50682a68c2e86bab9dcec0546344573499634
preshu1 <shresthapreshu26@gmail.com>
2025-11-24 22:09:37 +0545

    fetched data and displayed it on the webpage

--- js/script.js
+++ js/script.js
@@ -1,37 +1,11 @@
-// script.js
-// const apiKey = "bbFyPX+IBtoOPyPU6NBDWQ==9sYv7PN3isa";
+const product = document.querySelector("#products");
 
-// async function getJokes(count) {
-//   for (let i = 0; i < count; i++) {
-//     try {
-//       const response = await fetch("https://api.api-ninjas.com/v1/dadjokes", {
-//         headers: { "X-Api-Key": apiKey },
-//       });
-//       const data = await response.json();
-
-//       // Safe check
-//       if (Array.isArray(data) && data.length > 0 && data[0].joke) {
-//         console.log(`${i + 1}: ${data[0].joke}`);
-//       } else if (data.error) {
-//         console.log(`API Error: ${data.error}`);
-//       } else {
-//         console.log(`Unexpected response:`, data);
-//       }
-//     } catch (error) {
-//       console.log(`Error fetching joke #${i + 1}:`, error);
-//     }
-//   }
-// }
-
-// // Fetch 5 jokes
-// getJokes(5);
-const apiKey = "bbFyPX+IBtoOPyPU6NBDWQ==9sYv7SJst7PN3isa";
-
-fetch("https://api.api-ninjas.com/v1/animals?name=cat", {
-  headers: { "X-Api-Key": apiKey },
-})
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => {
-    console.log("Error caught!", error);
-  });
+fetch("https://dummyjson.com/products/1")
+  .then((res) => res.json())
+  .then((data) => {
+    product.innerHTML = `<h2>${data.title}</h2>
+    <p>${data.description}</p>
+    <p><strong>Price: </strong> ${data.price}</p>
+    <img src="${data.thumbnail}" width="200">`;
+  })
+  .catch((error) => console.log("Error!", error));
