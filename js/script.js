e6f60c5073fe3393375850d8b1402e212d3f907c
preshu1 <shresthapreshu26@gmail.com>
2025-11-24 22:26:57 +0545

    fetched and displayed multiple products

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 const product = document.querySelector("#products");
 
-fetch("https://dummyjson.com/products/1")
+fetch("https://dummyjson.com/products?limit=5")
   .then((res) => res.json())
   .then((data) => {
-    product.innerHTML = `<h2>${data.title}</h2>
-    <p>${data.description}</p>
-    <p><strong>Price: </strong> ${data.price}</p>
-    <img src="${data.thumbnail}" width="200">`;
+    product.innerHTML = data.products
+      .map(
+        (product) => `<h2>${product.title}</h2>
+    <p>${product.description}</p>
+    <p><strong>Price: </strong> ${product.price}</p>
+    <img src="${product.thumbnail}" width="200">`
+      )
+      .join("");
   })
   .catch((error) => console.log("Error!", error));
