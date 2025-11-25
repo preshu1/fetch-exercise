6ddf887f3c0dc513067e3ed4c2f9d7056ee8bbe8
preshu1 <shresthapreshu26@gmail.com>
2025-11-25 19:49:00 +0545

    fetched multiple user data and displayed it on the webpage

--- js/script.js
+++ js/script.js
@@ -14,20 +14,24 @@
 //   })
 //   .catch((error) => console.log("Error!", error));
 
-fetch("https://dummyjson.com/users/1")
+fetch("https://dummyjson.com/users?limit=5")
   .then((response) => response.json())
   .then((data) => {
-    const html = `<div id="ouruser">
-    <h2>Our Users</h2>
-    <h3>Id: ${data.id}</h3>
-    <h3>First Name: ${data.firstName}</h3>
-    <h3>Maiden Name: ${data.maidenName}</h3>
-    <h3>Last Name: ${data.lastName}</h3>
-    <h4>Age:${data.age}</h4>
-    </div>`;
+    const html = data.users
+      .map(
+        (user) => `<div id="ouruser">
+    <h3>Id: ${user.id}</h3>
+    <h3>First Name: ${user.firstName}</h3>
+    <h3>Maiden Name: ${user.maidenName}</h3>
+    <h3>Last Name: ${user.lastName}</h3>
+    <h4>Age:${user.age}</h4>
+    </div>`
+      )
+      .join("");
 
     const users = document.querySelector("#users");
 
-    users.innerHTML = html;
+    users.innerHTML = `
+    ${html}`;
   })
   .catch((error) => console.log("Error occured", error));
